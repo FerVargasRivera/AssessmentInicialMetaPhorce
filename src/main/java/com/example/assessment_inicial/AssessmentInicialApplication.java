94462b9531b7090bec9c8bf163218e38db0e2fc6
FernandoVaRa <fernando.vargas@isystems.mx>
2024-03-23 14:59:46 -0600

    Commit inicial

---
+++ src/main/java/com/example/assessment_inicial/AssessmentInicialApplication.java
@@ -0,0 +1,13 @@
+package com.example.assessment_inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentInicialApplication.class, args);
+	}
+
+}
